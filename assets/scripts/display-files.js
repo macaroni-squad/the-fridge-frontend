27a9e6346e755ee61086e25ece0000a2a586857a
JenWeber <weberj10@gmail.com>
2016-03-14 19:56:36 -0400

    clean up old comments and code

--- assets/scripts/display-files.js
+++ assets/scripts/display-files.js
@@ -2,7 +2,6 @@
 
 const globalObjects = require('./global-objects');
 
-
 let displayFolders = function(folders){
   let foldersTemplate = require('./folders.handlebars');
   console.log("displayFolders was callled");
@@ -24,6 +23,12 @@ const makeUnique = function(folders) {
     return folders.indexOf(elem) === pos;
   });
   return uniqueFolders;
+};
+
+let displayFolders = function(folders){
+  let foldersTemplate = require('./folders.handlebars');
+  console.log("displayFolders was callled");
+  $('.files-container').append(foldersTemplate({ folders }));
 };
 
 const assignFoldersTo = function (files) {
@@ -50,44 +55,6 @@ let displayFiles = function(files){
 };
 
 
-// const extractLocations = function(files) {
-//   let downloadLinks = [];
-//   files.forEach(function(file) {
-//     downloadLinks.push(file.location);
-//   });
-//   return downloadLinks;
-// };
-
-
-// const showFilesByFolder = function(files) {
-//   let downloadLinks = extractLocations(files);
-//   let folders = extractFolders(files);
-//   // let titles = extractTitles(files);
-//   folders.forEach(function(folder) {
-//     displayFolder(folder);
-//     let filesInThisFolder = [];
-//     downloadLinks.forEach(function(downloadLink) {
-//       if ((downloadLink.split("/"))[3] === folder) {
-//         filesInThisFolder.push(downloadLink);
-//         displayFiles(filesInThisFolder, folder);
-//       }
-//     });
-//   });
-// };
-
-
-const showFilesByFolder = function(files) {
-  // let downloadLinks = extractLocations(files);
-  let filesInThisFolder = [];
-  // let folder = extractFolders(files);
-  displayFiles(files);
-  // downloadLinks.forEach(function(downloadLink) {
-  //   if ((downloadLink.split("/"))[3] === folder) {
-  //     filesInThisFolder.push(downloadLink);
-  //
-  //   }
-};
-
 let getFiles = function() {
   $.ajax({
     url: 'http://localhost:3000' + '/files/',
@@ -97,15 +64,11 @@ let getFiles = function() {
     // },
     dataType: 'json'
   }).done(function(response){
-    // console.log(response);
-    // console.log(response.files);
-    showFilesByFolder(response.files);
-    // fileByTitle(response.files);
+    console.log("this get was called")
+    displayFiles(response.files);
   }).fail(function(jqxhr) {
     console.error(jqxhr);
   });
 };
-
-// getFiles();
 
 module.exports = getFiles;
